fce45af27d5bb27a7aa9e7d259daddf6e7f5b6e9
gaochong <gaochng@qq.com>
2021-10-26 23:42:28 +0800

    feat: init

--- src/commands/Start.php
+++ src/commands/Start.php
@@ -3,6 +3,7 @@
 
 namespace gc\ser\commands;
 
+use gc\ser\facades\App;
 use gc\ser\facades\Safe;
 use SimpleCli\Options\Help;
 use SimpleCli\Command;
@@ -14,8 +15,30 @@ class Start implements Command
 
     public function run(SimpleCli $cli): bool
     {
+        $this->init();
 
 
         return true;
+    }
+
+    private function init()
+    {
+        if (file_exists(App::runPidPath())){
+            $masterPid = file_get_contents(App::runPidPath());
+        }else{
+            $masterPid = 0;
+        }
+
+        // 验证当前进程是否存在
+        if (
+            $masterPid &&
+            posix_kill($masterPid,0) &&
+            $masterPid != posix_getpid()
+        ){
+            exit("server already running...");
+        }else{
+            unlink(App::runPidPath());
+        }
+
     }
 }
